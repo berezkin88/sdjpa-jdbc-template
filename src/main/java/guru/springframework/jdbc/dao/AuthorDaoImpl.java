8b98299eb365e37ae8d0d4c875c2bb65e22d1a45
berezkin88 <berezkin05@gmail.com>
2021-12-27 23:20:27 +0200

    added dao stubs

---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -0,0 +1,32 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AuthorDaoImpl implements AuthorDao {
+    @Override
+    public Author getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Author getByFirstAndLastName(String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+
+    }
+}
---
+++ src/test/java/guru/springframework/jdbc/dao/AuthorDaoImplTest.java
@@ -0,0 +1,75 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"com/example/sdjpajdbc/dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AuthorDaoImplTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Test
+    void testGetAuthorById() {
+        var author = authorDao.getById(1L);
+
+        assertThat(author).isNotNull();
+    }
+
+    @Test
+    void authorDao_findByFirstAndLastNames() {
+        var firstName = "Craig";
+        var lastName = "Walls";
+        var author = authorDao.getByFirstAndLastName(firstName, lastName);
+
+        assertThat(author)
+            .isNotNull();
+    }
+
+    @Test
+    void authorDao_saveNewAuthor() {
+        var author = new Author();
+        author.setLastName("Berezkin");
+        author.setFirstName("Oleksandr");
+        var savedAuthor = authorDao.saveNewAuthor(author);
+
+        assertThat(savedAuthor).isNotNull();
+    }
+
+    @Test
+    void authorDao_updateAuthor() {
+        var author = new Author();
+        author.setLastName("B");
+        author.setFirstName("Oleksandr");
+        var savedAuthor = authorDao.saveNewAuthor(author);
+
+        savedAuthor.setLastName("Berezkin");
+        var updatedAuthor = authorDao.updateAuthor(savedAuthor);
+
+        assertThat(updatedAuthor.getLastName()).isEqualTo("Berezkin");
+    }
+
+    @Test
+    void authorDao_deleteAuthorById() {
+        var author = new Author();
+        author.setLastName("B");
+        author.setFirstName("Oleksandr");
+        var savedAuthor = authorDao.saveNewAuthor(author);
+
+        authorDao.deleteAuthorById(savedAuthor.getId());
+
+        var deletedAuthor = authorDao.getById(savedAuthor.getId());
+        assertThat(deletedAuthor).isNull();
+    }
+
+}
